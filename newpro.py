df963707fbee87a41dde3c0fcf9208d04c92ecc5
fhhang <james@JamesdeMacBook-Pro.local>
2017-03-05 17:36:46 +0800

    第一次提交

---
+++ newpro.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+import os
+
+
+def main():
+    print("cyrrebt directory is", os.getcwd())
+    for i in range(5):
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
